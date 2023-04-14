4135df4783da4a31999553f62a965e2c2365742a
Hasnaat-Iftikhar <hasnaatfreelancing@gmail.com>
2023-04-14 16:04:32 +0500

    fix: Remove style attribute from div element

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main>
         <h1>Kicks - Ecommerce</h1>
       </main>
     </div>
