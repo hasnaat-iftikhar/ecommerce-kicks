8a0391b1b643439e02699af44ae2ada578683b55
Hasnaat-Iftikhar <hasnaatfreelancing@gmail.com>
2023-04-14 23:09:59 +0500

    feat: Implemented global color scheme and add required images

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,22 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#4A69E2",
+        yellow: "#FFA52F",
+        white: "#FFFFFF",
+        fa_white: "#FAFAFA",
+        gray: "#E7E7E3",
+        gray_main: "#70706E",
+        gray_dark: "#232321",
+      },
+    },
   },
   plugins: [],
-}
+};
