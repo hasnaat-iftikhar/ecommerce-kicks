99ca898f6de6f532d6240ac5f27e7058a7bb1382
Hasnaat Iftikhar <hasnaatfreelancing@gmail.com>
2023-05-27 12:30:09 +0500

    fix: Did ui fixings

--- components/layout/productCardsWrapper/index.jsx
+++ components/layout/productCardsWrapper/index.jsx
@@ -3,9 +3,20 @@ import React from "react";
 // Components
 import ProductCard from "../productCard";
 
-const ProductCardsWrapper = ({ productsData, columns = 4, className }) => {
+const ProductCardsWrapper = ({
+  productsData,
+  columns = 4,
+  gap = "16px",
+  className = "",
+}) => {
+  const gridStyles = {
+    display: "grid",
+    gridTemplateColumns: `repeat(${columns}, minmax(0, 1fr))`,
+    gap: gap,
+  };
+
   return (
-    <div className={`grid grid-cols-${columns} gap-4 ${className ?? ""}`}>
+    <div style={gridStyles} className={className}>
       {productsData?.map((d, i) => (
         <ProductCard image={d.image} name={d.name} price={d.price} key={i} />
       ))}
