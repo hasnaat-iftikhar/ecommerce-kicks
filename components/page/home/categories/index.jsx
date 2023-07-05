e228662b0da8ce711123a049a1e3edee6aba0a50
Hasnaat Iftikhar <hasnaatfreelancing@gmail.com>
2023-07-05 14:00:00 +0500

    feat: Worked on categories section responsive view

--- components/page/home/categories/index.jsx
+++ components/page/home/categories/index.jsx
@@ -37,13 +37,23 @@ const CategoriesSection = ({ className }) => {
           Categories
         </Text>
 
-        <Splide options={{ perPage: 2, perMove: 1 }}>
+        <Splide
+          options={{
+            perPage: 2,
+            perMove: 1,
+            breakpoints: {
+              640: {
+                perPage: 1,
+              },
+            },
+          }}
+        >
           {categoriesInfo?.map((s, i) => (
             <SplideSlide
               key={i}
               className={`${
                 i % 2 === 0 ? "bg-[#ECEEF0]" : "bg-[#F6F6F6]"
-              } relative w-[38vw] h-[50vw] md:h-[46vw] lg:h-[40vw] xxl:h-[600px]`}
+              } relative w-[38vw] h-[100vw] sm:h-[50vw] md:h-[46vw] lg:h-[40vw] xxl:h-[600px]`}
             >
               <Image
                 src={s.image}
@@ -52,10 +62,10 @@ const CategoriesSection = ({ className }) => {
                 className="px-[30px] md:px-[50px] lg:px-[70px] xl:px-[100px]"
               />
               <div className="flex flex-row justify-between items-end absolute bottom-[30px] w-full px-[40px] md:px-[48px]">
-                <h4 className="w-[min-content] text-[3vw] leading-[3.2vw] lg:text-[28px] lg:leading-[28px] xl:text-[30px] xl:leading-[30px] xxl:text-4xl font-semibold text-gray_dark uppercase">
+                <h4 className="w-[min-content] text-[6vw] leading-[6.2vw] sm:text-[3vw] sm:leading-[3.2vw] lg:text-[28px] lg:leading-[28px] xl:text-[30px] xl:leading-[30px] xxl:text-4xl font-semibold text-gray_dark uppercase">
                   {s.name}
                 </h4>
-                <button className="w-[36px] h-[36px] md:w-[44px] md:h-[44px] xl:w-[48px] xl:h-[48px] rounded-lg flex justify-center items-center cursor-pointer bg-gray_dark ">
+                <button className="w-[46px] h-[46px] sm:w-[36px] sm:h-[36px] md:w-[44px] md:h-[44px] xl:w-[48px] xl:h-[48px] rounded-lg flex justify-center items-center cursor-pointer bg-gray_dark ">
                   <Image
                     className="w-[24px] h-[24px] md:w-[32px] md:h-[32px] cursor-pointer"
                     src={RedirectIcon}
